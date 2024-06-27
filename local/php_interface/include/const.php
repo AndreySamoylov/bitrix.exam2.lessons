097ce9e19072289d1907ee7c8a0f2aab87242bd2
Андрей Самойлов <san@d-element.ru>
2024-06-27 16:54:49 +0500

    задание 3 пункт 1

--- local/php_interface/include/const.php
+++ local/php_interface/include/const.php
@@ -1,3 +1,4 @@
 <?php
 const IBLOCK_STOCK = '5';
+const IBLOCK_NEWS = '1';
 const ID_ADMINISTRATION_GROUP = '1';
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,4 @@
 <?
 require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/const.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/agent.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/event_handlers.php';
